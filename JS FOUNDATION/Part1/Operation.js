722a9379d4898aa4de92eee69904f3f9d258464d
Aditya Kumar <adityakr123muz@gmail.com>
2025-08-15 14:49:10 +0530

    Added operation"s

---
+++ JS FOUNDATION/Part1/Operation.js
@@ -0,0 +1,44 @@
+const Num = 12
+const Num1 = 10
+
+// Addition
+console.log(Num + Num1);
+
+// Subtract
+console.log(Num - Num1);
+
+// Multi
+console.log(Num * Num1);
+
+// divi
+console.log(Num / Num1);
+
+// Reminder
+
+console.log(Num % Num1);
+
+const expo = 2 ** 4
+console.log(expo);
+
+
+
+//  <-------------comparion operation------------->
+
+
+let Score = 120
+let Score1 =  122
+
+//  (==)
+console.log(Score ==  Score1);
+
+//  (!=)
+console.log(Score !=  Score1);
+
+//  (>)
+console.log(Score <  Score1);
+
+//  (<)
+console.log(Score >  Score1);
+
+
+
