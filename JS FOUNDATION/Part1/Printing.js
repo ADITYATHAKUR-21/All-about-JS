de34aee218b54e0d651536881324d824cd49445f
Aditya Kumar <adityakr123muz@gmail.com>
2025-08-15 13:27:08 +0530

    Added printing method's

---
+++ JS FOUNDATION/Part1/Printing.js
@@ -0,0 +1,8 @@
+// These are the printing method
+
+console.log("Printing method's");
+process.stdout.write("2nd printing method");
+
+
+console.table({Year: "Number"});
+console.warn({Name: "Aditya"})
