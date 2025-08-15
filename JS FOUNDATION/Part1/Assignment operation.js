a85b10fdfb2d3206e131d57190adc85352719456
Aditya Kumar <adityakr123muz@gmail.com>
2025-08-15 15:15:57 +0530

    Added asignment operation

---
+++ JS FOUNDATION/Part1/Assignment operation.js
@@ -0,0 +1,27 @@
+//  &&, Or, Reverse
+
+let Num1 = 120;
+let Num2 = 150
+
+//  && ----> And
+console.log(Num1 == Num2 && Num1 === Num2);
+
+//  || -----> Or
+console.log(Num1 == Num2 || Num1 === Num2);
+
+
+//  ! ------> Reverse
+
+console.log(Num1 != Num2);
+
+
+Num1 += 5;
+console.log(Num1);
+
+Num1 -= 5;
+console.log(Num1);
+
+//  <------------Assignment Operation ----------->
+
+let operation = (((2 * 2 )+ 2) - 1)
+console.log(operation);
