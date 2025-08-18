193e205379c8b0ed42f9b3bbbb1d43523ba07b01
Aditya Thakur <adityakr123muz@gmail.com>
2025-08-18 03:26:57 -0800

    Added Array in Nonprimitive

--- JS FOUNDATION/Part2/NonPrimitive.js
+++ JS FOUNDATION/Part2/NonPrimitive.js
@@ -3,12 +3,25 @@ const Username =  {
   Password : 589051
 }
 
-Username.username= " Rohan"
-console.log(Username['user name']);
-console.log(Username.Password);
-console.log(typeof Username);
+// Username.username= " Rohan"
+// console.log(Username['user name']);
+// console.log(Username.Password);
+// console.log(typeof Username);
 
 
 const today =  new Date();
 
-console.log(today.getFullYear());
+// console.log(today.getFullYear());
+
+
+
+//  Array
+
+const Worker =  ["Aditya", "Rohan", 5, null, true]
+
+
+console.log( typeof Worker[4]);
+
+let isValue = "1";
+
+console.log(isValue + 1);
