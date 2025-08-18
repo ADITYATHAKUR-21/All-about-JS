12eeff86f31fef8b0971729e381963131a19bdb9
Aditya Thakur <adityakr123muz@gmail.com>
2025-08-18 04:19:46 -0800

    Added If-Else

---
+++ JS FOUNDATION/Part3/If-Else.js
@@ -0,0 +1,60 @@
+// 1st
+
+let Num1 = 2;
+let Num2 =  4;
+
+if (Num1 < Num2) {
+  // console.log("Num1 is less than Num2");
+
+} else {
+  console.log("Nothing");
+
+
+}
+
+//  2nd
+
+let  firstString = "Aditya"
+let secondString = "Aditya"
+
+if(firstString === secondString){
+  console.log("Those strings value is same ");
+
+}else{
+  console.log("Those strings value is not same ");
+
+}
+
+// 3rd
+
+let value = 20
+
+if(typeof value == "number"){
+  console.log("Value store a number data type");
+
+}else{
+  console.log("Value dose not store a number data type");
+
+}
+
+// 4
+
+let isProject = true;
+
+if(isProject){
+  console.log("isProject is ture");
+
+}else{
+ console.log("isProject is not ture");
+}
+
+// 5
+
+let isArray =  [];
+
+if(isArray.length === 0){
+  console.log("Array is empty");
+
+}else{
+  console.log("Array is not empty");
+}
