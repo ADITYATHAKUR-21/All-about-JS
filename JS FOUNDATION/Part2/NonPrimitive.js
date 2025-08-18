a22d8d838d68508d4d28b6bc8809b8a3066bab75
Aditya Thakur <adityakr123muz@gmail.com>
2025-08-18 03:03:12 -0800

    Nonprimitive is done

--- JS FOUNDATION/Part2/NonPrimitive.js
+++ JS FOUNDATION/Part2/NonPrimitive.js
@@ -1,4 +1,14 @@
 const Username =  {
-  username: "Aditrya Thakur",
+  "user name": "Aditya Thakur",
   Password : 589051
 }
+
+Username.username= " Rohan"
+console.log(Username['user name']);
+console.log(Username.Password);
+console.log(typeof Username);
+
+
+const today =  new Date();
+
+console.log(today.getFullYear());
