88fcf6b249b0f1906c89841a62285a987d6b217d
Aditya Thakur <adityakr123muz@gmail.com>
2025-08-23 20:52:21 -0800

    Added for loop

---
+++ JS FOUNDATION/Part5/ForLoop.js
@@ -0,0 +1,21 @@
+// 1st
+
+let MultipliedNumbers = []
+let numbers = [2, 3, 6]
+
+for (let A = 0; A < numbers.length; A++) {
+  let takenum = numbers[A] * 2;
+  MultipliedNumbers.push(takenum)
+
+}
+// console.log(MultipliedNumbers);
+
+//  2nd
+ let cities = ["Patna", "Muzffarpur", "Bhaglpur", "Motihari"];
+ let city = [];
+
+ for(let L = 0; L < cities.length; L++){
+  city.push(cities[L]);
+
+ }
+  console.log(city);
